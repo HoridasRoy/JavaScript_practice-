b9883b6fc0acb65c19cc95ea4690d5c618f4f660
Horidas Roy <horidasr@gmail.com>
2017-10-28 00:18:55 +0600

    About js object

---
+++ object.js
@@ -0,0 +1,13 @@
+function myFunction() {
+	// body...
+
+		var person = {
+	    firstName: "John",
+	    lastName : "Doe",
+	    id       :  5566
+	};
+
+	document.getElementById("demo").innerHTML  =
+	person.firstName + " " + person.lastName;
+}
+
